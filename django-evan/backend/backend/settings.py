270b898fc7c56b92b4a55e08e9657a1a88f44720
Aastha Shah <aasthashahoppo2005@gmail.com>
2026-03-20 17:22:04 +0530

    Fixed

--- django-evan/backend/backend/settings.py
+++ django-evan/backend/backend/settings.py
@@ -144,7 +144,7 @@ SIMPLE_JWT = {
 }
 CORS_ALLOW_ALL_ORIGINS = True
 CORS_ALLOWED_ORIGINS = [
-    "http://localhost:5173",
+    "https://evanote-three.vercel.app",
 ]
 CORS_ALLOW_CREDENTIALS = True
 
