5fce76ceb92a7244c98ba0f92bbf821bacca3ba0
Aastha Shah <aasthashahoppo2005@gmail.com>
2026-03-30 02:35:49 +0530

    Your commit message

--- django-evan/backend/evan/apps.py
+++ django-evan/backend/evan/apps.py
@@ -1,18 +1,20 @@
 from django.apps import AppConfig
 
-
 class EvanConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'evan'
 
     def ready(self):
+        from django.db.models.signals import post_migrate
         from django.contrib.auth import get_user_model
 
-        User = get_user_model()
+        def create_superuser(sender, **kwargs):
+            User = get_user_model()
+            if not User.objects.filter(username='admin').exists():
+                User.objects.create_superuser(
+                    username='admin',
+                    email='admin@example.com',
+                    password='admin123'
+                )
 
-        if not User.objects.filter(username='admin').exists():
-            User.objects.create_superuser(
-                username='dokja',
-                email='aasthashahoppo2005@gmail.com',
-                password='ilovejunghyeok'
-            )
+        post_migrate.connect(create_superuser, sender=self)
